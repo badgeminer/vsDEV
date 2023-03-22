d3196b93dccac38af88acb4488568ffd7b9b9235
badgeminer <64038298+badgeminer@users.noreply.github.com>
2023-03-22 07:50:29 -0600

    lots

--- script.js
+++ script.js
@@ -9,40 +9,113 @@ function clear() {
 const hud = {
   money:{
     hudX: 10,
-    hudY: 10
+    hudY: 10,
+    drw: function() {
+
+      ctx.beginPath();
+      ctx.lineWidth = 2;
+      ctx.fillStyle = "#000000";
+      ctx.strokeStyle = "#009933";
+      ctx.roundRect(hud.money.hudX, hud.money.hudY, 85, 28, 5);
+      ctx.stroke();
+      ctx.fill()
+
+      ctx.fillStyle = "#ffffff";
+      ctx.font = "13px Space Mono, monospace";
+      ctx.fillText(" Money:", hud.money.hudX, hud.money.hudY+10);
+      var str = Math.round(Game.money).toString()
+      str = str.padStart(9, "0")
+      ctx.fillText(` $${str} `, hud.money.hudX, hud.money.hudY+22.5);
+    }
+  },
+  dev:{
+    hudX: 10,
+    hudY: 50,
+    drw: function() {
+      if (Game.flags.devMode) {
+        ctx.beginPath();
+        ctx.lineWidth = 2;
+        ctx.fillStyle = "#000000";
+        ctx.strokeStyle = "#009933";
+        ctx.roundRect(hud.dev.hudX, hud.dev.hudY, 170, 58, 5);
+        ctx.stroke();
+        ctx.fill()
+
+        ctx.fillStyle = "#ffffff";
+        ctx.font = "13px Space Mono, monospace";
+        ctx.fillText(" Money:", hud.dev.hudX, hud.dev.hudY+10);
+        var str = Math.round(Game.money).toString()
+        str = str.padStart(18, "0")
+        ctx.fillText(` $${str} `, hud.dev.hudX, hud.dev.hudY+22.5);
+      }
+    }
   }
 }
 
 var Game = {
   money:500,
-  assets: {}
+  assets: {},
+  flags: {
+    devMode:false
+  },
+  clicks: {
+    dev:{
+      sx:100,sy:10,
+      ex:130,ey:40,
+      act: function() {
+        Game.flags.devMode = !Game.flags.devMode
+      }
+    }
+  }
+}
+function isClickd(obj,x,y) {
+  //console.log((obj.sx < x))
+  //console.log((obj.sy < y))
+  //console.log((obj.ex > x))
+  //console.log((obj.ey > y))
+  if ((obj.sx < x) & (obj.sy < y) & (obj.ex > x) & (obj.ey > y)) {
+      
+          return true
+
+  }
+  return false
 }
 
 function ui() {
-  ctx.beginPath();
-  ctx.lineWidth = 2;
-  ctx.fillStyle = "#000000";
-  ctx.strokeStyle = "#009933";
-  ctx.roundRect(hud.money.hudX, hud.money.hudY, 85, 28, 5);
-  ctx.stroke();
-  ctx.fill()
+  ctx.drawImage(Game.assets.dev, 100, 10);
+  for (const property in hud) {
+    hud[property].drw()
+  }
+}
 
-  ctx.fillStyle = "#ffffff";
-  ctx.font = "13px Space Mono, monospace";
-  ctx.fillText(" Money:", hud.money.hudX, hud.money.hudY+10);
-  var str = Math.round(Game.money).toString()
-  str = str.padStart(9, "0")
-  ctx.fillText(` $${str} `, hud.money.hudX, hud.money.hudY+22.5);
+function getCursorPosition(canvas, event) {
+  const rect = canvas.getBoundingClientRect()
+  const x = event.clientX - rect.left
+  const y = event.clientY - rect.top
+  //console.log("x: " + x + " y: " + y)
+  for (const property in Game.clicks) {
+    if (isClickd(Game.clicks[property],x,y)) {
+      Game.clicks[property].act()
+    }
+  }
 }
 
 window.onload = function() {
   var c = document.getElementById("canvas");
+  
+
+  c.addEventListener('mousedown', function(e) {
+      getCursorPosition(c, e)
+  })
   ctx = c.getContext("2d");
   ctx.canvas.width = window.innerWidth - 15;
   ctx.canvas.height = window.innerHeight - 15;
   Game.assets.dev = document.getElementById("dev");
   clear();
   ui();
-  
+  interv = setInterval(function() {
+    clear();
+    ui();
+  },500)
 }
 
